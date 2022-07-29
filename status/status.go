ceeb3aa78f8784f3ba5a313c5201de6f8c2e7220
Javier <javi_mr@me.com>
2022-07-29 16:56:03 +0200

    Cleaned status.go

--- status/status.go
+++ status/status.go
@@ -4,6 +4,13 @@ import (
 	"strings"
 )
 
+var (
+	yourPRsMsg     = "Created by you"
+	codeReviewMsg  = "\nRequesting a code review from you\n"
+	noReviewPRsMsg = "You have no pull requests to review\n\n"
+	noOpenedPRsMsg = "\n  You have no open pull requests\n"
+)
+
 type PR struct {
 	ID          int    `json:"id"`
 	Name        string `json:"name"`
@@ -17,26 +24,25 @@ type RepoStatus struct {
 	ReviewPRs []PR   `json:"review_prs"`
 }
 
-var (
-	noReviewPRs = "You have no pull requests to review\n\n"
-	noOpenedPRs = "\n  You have no open pull requests\n"
-)
-
-func GetStatus(repo string) (RepoStatus, error) {
+// Retrieves the status for all the PRs in the repo
+// related to the user making the request. Returns
+// error if the statuses cannot be retrieved or parsed
+// successfully.
+func GetRepoStatus(repo string) (RepoStatus, error) {
 	stdout, err := ghPRStatus(repo)
 	if err != nil {
 		return RepoStatus{}, err
 	}
 
-	info := strings.Split(stdout, "Created by you")[1]
-	infos := strings.Split(info, "\nRequesting a code review from you\n")
+	info := strings.Split(stdout, yourPRsMsg)[1]
+	infos := strings.Split(info, codeReviewMsg)
 
-	oPRs, err := extractPRs(infos[0], noOpenedPRs)
+	oPRs, err := extractPRs(infos[0], noOpenedPRsMsg)
 	if err != nil {
 		return RepoStatus{}, err
 	}
 
-	rPRs, err := extractPRs(infos[1], noReviewPRs)
+	rPRs, err := extractPRs(infos[1], noReviewPRsMsg)
 	if err != nil {
 		return RepoStatus{}, err
 	}
