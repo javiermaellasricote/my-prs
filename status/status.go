f32e71bdc0ce73c0a5e46014cf041a15958a566c
Javier <javi_mr@me.com>
2022-07-29 15:42:01 +0200

    Parsing status

--- status/status.go
+++ status/status.go
@@ -3,12 +3,14 @@ package status
 import (
 	"bytes"
 	"errors"
+	"fmt"
 	"os/exec"
+	"strconv"
 	"strings"
 )
 
 type PR struct {
-	ID          int32  `json:"id"`
+	ID          int    `json:"id"`
 	Name        string `json:"name"`
 	Description string `json:"description"`
 	Status      string `json:"status"`
@@ -36,13 +38,25 @@ func getOpenPRs(prInfo string) ([]PR, error) {
 	prs := make([]PR, len(data)/4)
 	for i, item := range data {
 		idx := i / 4
+		cleanItem := strings.Trim(item, " ")
+
 		switch i % 4 {
 		case 0:
 			continue
 		case 1:
-			//TODO: Get the name, description and id
+			fmt.Println(cleanItem)
+			split1 := strings.Split(cleanItem, "#")[1]
+			split2 := strings.Split(split1, "  ")
+			split3 := strings.Split(split2[1], " [")
+			id, err := strconv.Atoi(split2[0])
+			if err != nil {
+				return []PR{}, err
+			}
+			prs[idx].ID = id
+			prs[idx].Description = split3[0]
+			prs[idx].Name = strings.Trim(split3[1], "]")
 		case 2:
-			prs[idx].Status = item
+			prs[idx].Status = cleanItem
 		case 3:
 			continue
 		default:
