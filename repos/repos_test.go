08660533afd2ef6e2daf116b58f609178ab67911
Javier <javi_mr@me.com>
2022-08-28 19:02:26 +0200

    Started adding tests

---
+++ repos/repos_test.go
@@ -0,0 +1,13 @@
+package repos
+
+import "testing"
+
+func TestGetRepos(t *testing.T) {
+	cases := []struct {
+		name      string
+		owner     string
+		repoLimit string
+		err       error
+		resp      []string
+	}{}
+}
