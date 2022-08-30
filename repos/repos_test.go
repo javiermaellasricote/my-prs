5c2740144af02d14aaaccda2432decfb9ad63ec4
Javier <javi_mr@me.com>
2022-08-30 09:25:54 +0200

    Created frame for test table

--- repos/repos_test.go
+++ repos/repos_test.go
@@ -1,13 +1,29 @@
 package repos
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestGetRepos(t *testing.T) {
 	cases := []struct {
 		name      string
 		owner     string
-		repoLimit string
+		repoLimit int
 		err       error
-		resp      []string
+		res       []string
 	}{}
+
+	for _, tt := range cases {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			res, err := GetRepos(tt.owner, tt.repoLimit)
+			if err != tt.err {
+				t.Errorf("Expected error %v, but received %v", tt.err, err)
+			}
+			if !reflect.DeepEqual(res, tt.res) {
+				t.Errorf("Expected result %v, but received %v", tt.res, res)
+			}
+		})
+	}
 }
